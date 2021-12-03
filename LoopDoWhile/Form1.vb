deb2239f8a9af4a2c48b59a9971a6f3b70462132
Anderson <Anderson@DESKTOP-NC5OC9G>
2021-12-03 17:25:48 -0300

    Add project files.

---
+++ LoopDoWhile/Form1.vb
@@ -0,0 +1,22 @@
+﻿Public Class Form1
+    Private Sub btnListar_Click(sender As Object, e As EventArgs) Handles btnListar.Click
+
+        limparLista()
+
+        Dim i As Integer
+
+        Do While i <= 5
+            lstLista.Items.Add("Número " & i & " adicionado!")
+            i += 1
+        Loop
+
+    End Sub
+
+    Private Sub limparLista()
+        lstLista.Items.Clear()
+    End Sub
+
+    Private Sub btnLimpar_Click(sender As Object, e As EventArgs) Handles btnLimpar.Click
+        limparLista()
+    End Sub
+End Class
